f7ae74b57d3fcbbd7745a9788006a2313e92ea55
Patrik0423 <patrik.jagodits@i-ce.eu>
2023-11-27 10:24:45 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';  // Import FormsModule
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule  // Add FormsModule here
+  ],
+  providers: [],
+  bootstrap: [AppComponent]  // Add the component you want to bootstrap
+})
+export class AppModule { }
